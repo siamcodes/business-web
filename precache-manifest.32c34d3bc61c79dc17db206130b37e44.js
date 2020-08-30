cde51e7a8f34340abf0bcd46e972ca40fd6b8c5f
siamcodes <siamcodes@gmail.com>
2020-08-30 12:17:28 +0700

    Updates

---
+++ precache-manifest.32c34d3bc61c79dc17db206130b37e44.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4724f20d13f2017bc204d4ca6a38a38c",
+    "url": "/business-web/index.html"
+  },
+  {
+    "revision": "873161b6746ff6b98490",
+    "url": "/business-web/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "163c792b771cd1732eb3",
+    "url": "/business-web/static/css/main.017eec5e.chunk.css"
+  },
+  {
+    "revision": "873161b6746ff6b98490",
+    "url": "/business-web/static/js/2.4cd3cca4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/business-web/static/js/2.4cd3cca4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "163c792b771cd1732eb3",
+    "url": "/business-web/static/js/main.433eaf8c.chunk.js"
+  },
+  {
+    "revision": "37942b4553f31021b113",
+    "url": "/business-web/static/js/runtime-main.ce90de8e.js"
+  }
+]);
